f45fcb4c7ce2ca47248692ffca7e38c6ac3b0743
theEndless11 <aaa380144@gmail.com>
2025-03-07 15:41:29 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -8,7 +8,7 @@ const pool = new Pool({
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
-    port: 5432,
+    port: 18395,
 });
 
 module.exports = pool;
