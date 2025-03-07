3d2be3bd7d67978ba1b4da374a6276de857ad21b
theEndless11 <aaa380144@gmail.com>
2025-03-07 16:01:52 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -4,11 +4,12 @@ const { Pool } = require('pg');
 
 // Create a PostgreSQL connection pool
 const pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: 18395,
+  host: process.env.DB_HOST, // Aiven host URL
+  port: process.env.DB_PORT, // Port number
+  user: process.env.DB_USER, // Database username
+  password: process.env.DB_PASSWORD, // Database password
+  database: process.env.DB_DATABASE, // Database name
+  ssl: { rejectUnauthorized: false }, // SSL configuration (required for Aiven and cloud providers)
 });
 
 module.exports = pool;
